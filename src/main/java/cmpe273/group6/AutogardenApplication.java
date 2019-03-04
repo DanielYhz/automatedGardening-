1f1f292949e4341544a885dc2057980c8f357294
Daniel Yang <achillesyang@hotmail.com>
2019-03-03 23:07:30 -0800

    Data init
    Data init

---
+++ src/main/java/cmpe273/group6/AutogardenApplication.java
@@ -0,0 +1,13 @@
+package cmpe273.group6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutogardenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutogardenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cmpe273/group6/autogarden/AutogardenApplicationTests.java
@@ -0,0 +1,16 @@
+package cmpe273.group6.autogarden;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutogardenApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
